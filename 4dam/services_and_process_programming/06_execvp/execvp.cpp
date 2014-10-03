1fe2ae3061eef22b0ae8413311338169c8d3e92b
smashkun <danielmarcos.to@gmail.com>
2014-10-03 10:44:31 +0200

    homeworks

--- 4dam/services_and_process_programming/06_execvp/execvp.cpp
+++ 4dam/services_and_process_programming/06_execvp/execvp.cpp
@@ -3,7 +3,18 @@
 
 int main(int argc, char *argv[]){
 
+    int child_status;
 
+    char arg_list[3];
+    arg_list = {"ls", "-l", "/", NULL};
 
-    return EXIT_SUCCESS;
+    spawn ("ls", arg_list);
+
+    wait (&child_status);
+    if(WIFEXITED (child_status))
+	printf("the child process exited normally, with xode %d\n", WIXITSTATUS (child_status));
+    else
+	printf("the child process exited abnormally\n");
+
+    return 0;
 }
