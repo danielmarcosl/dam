8a83f4e90a1a8caf9bb108397a05f9544a9cf8c7
smash <danielmarcos.to@gmail.com>
2014-10-04 21:58:32 +0200

    homeworks done

--- 4dam/services_and_process_programming/05_execvp/execvp.cpp
+++ 4dam/services_and_process_programming/05_execvp/execvp.cpp
@@ -1,20 +1,36 @@
 #include <stdio_ext.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int spawn (char* program, char** arg_list)
+{
+    pid_t child_pid;
+
+    child_pid = fork();
+
+    if(child_pid != 0)
+	return child_pid;
+    else
+    {
+	execvp (program, arg_list);
+	fprintf (stderr, "an error occurred in execvp\n");
+	abort ();
+    }
+}
 
 int main(int argc, char *argv[]){
 
-    int child_status;
-
-    char arg_list[3];
-    arg_list = {"ls", "-l", "/", NULL};
+    char* arg_list[] = 
+    {
+	"ls",
+	"-l",
+	"/",
+	NULL
+    };
 
     spawn ("ls", arg_list);
-
-    wait (&child_status);
-    if(WIFEXITED (child_status))
-	printf("the child process exited normally, with xode %d\n", WIXITSTATUS (child_status));
-    else
-	printf("the child process exited abnormally\n");
+    printf ("done with main program\n");
 
     return 0;
 }
