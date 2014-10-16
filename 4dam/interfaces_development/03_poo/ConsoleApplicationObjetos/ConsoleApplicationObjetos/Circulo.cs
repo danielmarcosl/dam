537cee1f0e15a3e7329cc9dbe47312c25aa3c9a7
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2014-10-16 12:59:04 +0200

    Upload from interfaces class

---
+++ 4dam/interfaces_development/03_poo/ConsoleApplicationObjetos/ConsoleApplicationObjetos/Circulo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplicationAreaCirculo
+{
+    class Circulo
+    {
+        // Atributos
+        private double radio;
+        public double coordX, coordY;
+
+        // Métodos
+
+        public Circulo()
+        {
+            radio = 2;
+            coordX = 10;
+            coordY = 50;
+        }
+
+        public Circulo(double r)
+        {
+            radio = r;
+            coordX = 10;
+            coordY = 50;
+        }
+
+        public Circulo(double r, double px, double py)
+        {
+            radio = r;
+            coordX = px;
+            coordY = py;
+        }
+
+
+        public double Area()
+        {
+            return (3.1416 * radio * radio);
+        }
+
+        public void CambiaRadio(double r)
+        {
+            radio = r;
+        }
+
+        public void Escribir()
+        {
+            Console.WriteLine("Radio: " + radio +
+                " PosX = " + coordX + " PosY = " + coordY);
+        }
+
+
+
+    }
+}
