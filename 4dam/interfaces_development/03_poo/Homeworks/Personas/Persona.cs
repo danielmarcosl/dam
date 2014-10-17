31db677d2b418d6878aa6e446b394dcb5f77fc05
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2014-10-17 11:32:54 +0200

    Upload homeworks

---
+++ 4dam/interfaces_development/03_poo/Homeworks/Personas/Persona.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Personas
+{
+    class Persona
+    {
+        // Atributos
+        private string nombre;
+        private int anioNac;
+        private string direccion;
+
+        // Constructores
+        public Persona()
+        {
+            nombre = "";
+            anioNac = 2000;
+            direccion = Madrid;
+        }
+
+        public Persona(string n, int a,string d)
+        {
+            nombre = "";
+            anioNac = 2000;
+            direccion = Madrid;
+        }
+    }
+}
