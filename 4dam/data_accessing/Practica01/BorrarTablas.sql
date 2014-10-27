f7a98dd35f96937c7394c8db785698589d268184
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2014-10-27 13:57:55 +0100

    Upload Practica01

---
+++ 4dam/data_accessing/Practica01/BorrarTablas.sql
@@ -0,0 +1,14 @@
+DROP TABLE Tiene;
+DROP TABLE Escribe;
+DROP TABLE Articulo;
+DROP TABLE Libro;
+DROP TABLE Publicacion;
+DROP TABLE Autor;
+DROP TABLE Editorial;
+DROP TABLE Compra;
+DROP TABLE Trabaja;
+DROP TABLE Empleado;
+DROP TABLE Turno;
+DROP TABLE cliente;
+DROP TABLE Factura;
+COMMIT;
