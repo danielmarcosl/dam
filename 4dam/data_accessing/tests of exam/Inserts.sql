d30b12059883ae7fa59b72f0c4b6f8d7f77404fd
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2014-11-26 01:19:00 +0100

    Upload sql

---
+++ 4dam/data_accessing/tests of exam/Inserts.sql
@@ -0,0 +1,36 @@
+insert into Alumnos values
+('A1','Pepe','Madrid');
+insert into Alumnos values
+('A2','Juan','Toledo');
+insert into Alumnos values
+('A3','Torres','Al Qaeda');
+insert into Alumnos values
+('A4','Manolo','Sevilla');
+
+insert into Asignaturas values
+('AS1','FOL','16');
+insert into Asignaturas values
+('AS2','RET','11');
+insert into Asignaturas values
+('AS3','Interfaces','19');
+insert into Asignaturas values
+('AS4','Bases','7');
+
+insert into Notas values
+('A1','AS1','1','1','1','4');
+insert into Notas values
+('A2','AS2','1','1','1','6');
+insert into Notas values
+('A3','AS2','1','1','1','5');
+insert into Notas values
+('A4','AS4','1','1','1','9');
+insert into Notas values
+('A1','AS3','1','1','1','8');
+insert into Notas values
+('A2','AS2','1','1','1','3');
+insert into Notas values
+('A3','AS1','1','1','1','1');
+insert into Notas values
+('A4','AS2','1','1','1','7');
+insert into Notas values
+('A1','AS4','1','1','1','4');
